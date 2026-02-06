d8469c1de3fb15d0f0f4d3c6d67a0f7e8c58547e
andrewfu1 <andrewfu100@gmail.com>
2026-02-06 15:32:06 -0500

    iou

--- src/tracking.py
+++ src/tracking.py
@@ -43,3 +43,26 @@ def extract_detection(labels: np.ndarray, points: np.ndarray, frame: int) -> lis
 
     return all_detections
 
+# This is 2d for now
+def calculate_iou(box1: BoundingBox, box2: BoundingBox) -> float:
+    # no overlap edge case
+    if box1.min_x >= box2.max_x or box1.min_y >= box2.max_y:
+        return 0.0
+    if box2.min_x >= box1.max_x or box2.min_y >= box1.max_y:
+        return 0.0
+    
+    # zero area edge case
+    box1_area = (box1.max_x - box1.min_x) * (box1.max_y - box1.min_y)
+    box2_area = (box2.max_x - box2.min_x) * (box2.max_y - box2.min_y)
+    if box1_area == 0 or box2_area == 0:
+        return 0.0
+
+    min_x, min_y = max(box1.min_x, box2.min_x), max(box1.min_y, box2.min_y)
+    max_x, max_y = min(box1.max_x, box2.max_x), min(box1.max_y, box2.max_y)
+    intersection_area = (max_x - min_x) * (max_y - min_y)
+
+    union_area = box1_area + box2_area - intersection_area
+
+    iou = intersection_area / union_area
+
+    return iou
